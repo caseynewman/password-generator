a87876adc7e94bd4854016ccdaa50ecb22eca8c5
Yogi Spice <caseynnewman@yahoo.com>
2023-12-06 12:51:41 -0500

    created prompts to get user criteria and if/else statements to make sure criteria was met

--- script.js
+++ script.js
@@ -15,7 +15,35 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
+
+
 const generatePassword = () => {
+  let password = ('');
+
+let hasSpecial = confirm("Would you like to include special characters?");
+let hasNumber = confirm("Would you like to include numbers?");
+let hasUppercase = confirm("Would you like to include uppercase letters?");
+let hasLowercase = confirm("Would you like to include lowercase letters?");
+let numOfCharacters = prompt("How many characters would you like your password to be? It must be between 8 and 128.");
+
+if (numOfCharacters < 8 || numOfCharacters > 128) {
+  alert("Your password must be between 8 and 128 characters in length.");
+} else if (hasSpecial === false && hasNumber === false && hasUppercase === false && hasLowercase === false) {
+  alert("Your password must include at least one special character, number, uppercase letter, or lowercase letter.")
+} else {
+  alert("Thank you! Your password will appear on the browser!");
+}
+
+let lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+let pwLength = [''];
+
+
+
+
+
   //hasSpecial
   //hasNumber
   //hasUppercase
@@ -33,21 +61,14 @@ const generatePassword = () => {
 // click generate -> prompts for criteria
 // at least 8 characters, no more than 128
 
-// confirm include lowercase?
-let lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-console.log(lowercase.length)
 
-// confirm include uppercase
-let uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-console.log(uppercase.length)
+// let password = ['lowercase', 'uppercase', 'numbers', 'special'];
 
-// confirm include numbers
-let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-console.log(numbers.length)
+let min = 8;
+let max = 128;
 
-// confirm include special characters
-let special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
-console.log(special.length)
+let password = Math.floor(Math.random) * (max - min)
+
 
 
 // store ^
@@ -55,5 +76,3 @@ console.log(special.length)
 
 // prompts are answered -> password generated to match criteria
 // password displayed in alert or written to page
-
-
