02a0019fc8842213345b7958d06e620b3a9eeb9d
Yogi Spice <caseynnewman@yahoo.com>
2023-12-05 22:29:27 -0500

    made arrays for each prompt, added comments

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
+
+// click generate -> prompts for criteria
+// at least 8 characters, no more than 128
+
+// confirm include lowercase?
+let lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+console.log(lowercase.length)
+
+// confirm include uppercase
+let uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+console.log(uppercase.length)
+
+// confirm include numbers
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+console.log(numbers.length)
+
+// confirm include special characters
+let specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+console.log(specialChar.length)
+
+
+// store ^
+// input should be validated and at least one character type should be selected
+
+// prompts are answered -> password generated to match criteria
+// password displayed in alert or written to page
+
+
