4126b9de5ca9e666366afa4dd6353d3554d82e78
Yogi Spice <caseynnewman@yahoo.com>
2023-12-08 13:55:22 -0500

    adding if statements

--- script.js
+++ script.js
@@ -17,31 +17,87 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-let lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-let special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
-let pwLength = [''];
+let lowercase = ['abcdefghijklmnopqrstuvwxyz'];
+let uppercase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+let numbers = ['0123456789'];
+let special = ['!@#$%^&*()+-./:;<=>?[]_`{|}~'];
+let multiSelect = [''];
+
+hasSpecNumUpperLower = 
+hasSpecNumUpper =
+hasSpecNumLower =
+hasSpec
 
 
 const generatePassword = () => {
   let password = ('');
 
 
-let hasSpecial = confirm("Would you like to include special characters?");
-let hasNumber = confirm("Would you like to include numbers?");
-let hasUppercase = confirm("Would you like to include uppercase letters?");
-let hasLowercase = confirm("Would you like to include lowercase letters?");
-let numOfCharacters = prompt("How many characters would you like your password to be? It must be between 8 and 128.");
+let hasSpecial = confirm("Click OK if you would like to include special characters.");
+let hasNumber = confirm("Click OK if you would like to include numbers.");
+let hasUpper = confirm("Click OK if you would like to include uppercase letters.");
+let hasLower = confirm("Click OK if you would like to include lowercase letters.");
+let pwLength = prompt("How many characters would you like your password to be? It must be between 8 and 128.");
 
 
-if (numOfCharacters < 8 || numOfCharacters > 128) {
+
+if (pwLength < 8 || pwLength > 128) {
   alert("Your password must be between 8 and 128 characters in length.");
-} else if (hasSpecial === false && hasNumber === false && hasUppercase === false && hasLowercase === false) {
+} else if (hasSpecial === false && hasNumber === false && hasUpper === false && hasLower === false) {
   alert("Your password must include at least one special character, number, uppercase letter, or lowercase letter.")
 } else {
   alert("Thank you! Your password will appear on the browser!");
 }
+
+
+if(hasSpecial && hasNumber && hasUpper && hasLower) {
+  return (lower)
+}
+
+if(hasSpecial && hasNumber && hasUpper && !hasLower) {
+
+}
+
+if(hasSpecial && hasNumber && !hasUpper && hasLower) {
+
+}
+
+if(hasSpecial && !hasNumber && hasUpper && hasLower) {
+
+}
+
+if(!hasSpecial && hasNumber && hasUpper && hasLower) {
+
+}
+
+if(hasSpecial && hasNumber && !hasUpper && !hasLower) {
+
+}
+
+if(hasSpecial && !hasNumber && !hasUpper && !hasLower) {
+
+}
+
+if(!hasSpecial && !hasNumber && !hasUpper && hasLower) {
+
+}
+
+if(hasSpecial && !hasNumber && !hasUpper && hasLower) {
+
+}
+
+if(!hasSpecial && !hasNumber && hasUpper && hasLower) {
+
+}
+
+if(!hasSpecial && hasNumber && !hasUpper && hasLower) {
+
+}
+
+if(!hasSpecial && hasNumber && hasUpper && !hasLower)
+
+
+
 
 
 
